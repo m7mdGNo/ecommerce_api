2cbf8bd078a29410863200bcc6168923d877062c
m7mdGNO <m7mdelganainy2001@gmail.com>
2022-09-26 07:41:10 +0100

    second one

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce_website.urls'
@@ -169,6 +170,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
