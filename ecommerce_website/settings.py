de1d8ffe15c2bd397dfb4d150bf99e59e14aaceb
m7mdGNO <m7mdelganainy2001@gmail.com>
2022-09-26 08:10:24 +0100

    second one

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -98,7 +98,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce_website.urls'
